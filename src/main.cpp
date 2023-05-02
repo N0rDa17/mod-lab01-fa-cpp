1affe832792de8cfa48af0488b7ea304adb5c492
N0rDa17 <114523335+N0rDa17@users.noreply.github.com>
2023-05-02 15:47:51 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
 int main() {
+    setlocale(LC_ALL, "Russian");
+    char str[100];
+    std::cout << "Введите текст: ";
+    std::cin.getline(str, 100);
+    std::cout << "Lab1.1: " << faStr1(str) << std::endl;
+    std::cout << "Lab1.2: " << faStr2(str) << std::endl;
+    std::cout << "Lab1.3: " << faStr3(str) << std::endl;
+    
     return 0;
 }
